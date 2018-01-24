f0288c02d80c09f28ee5e6d7c0c5e970a6848540
= <=>
2018-01-24 17:14:46 +0100

    files for project angularJS + spring MVC

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+var App = angular.module('AngularJSApp', []);
+
+const http = require('http');
+const fs = require('fs');
+const hostname = 'localhost';
+const port = 4200;
+fs.readFile('index.html', (err, html) => {
+    if (err) {
+        throw err;
+    }
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html');
+        res.write(html);
+        res.end();
+    });
+    server.listen(port, hostname, () => {
+        console.log('server started on port ' + port);
+    });
+});
